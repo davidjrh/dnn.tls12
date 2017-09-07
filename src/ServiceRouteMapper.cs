4e9cbc3c9923c0b04023c33324b300902b5d6884
David Rodriguez <davidj@intelequia.com>
2017-09-07 16:56:29 +0100

    Initial commit

---
+++ src/ServiceRouteMapper.cs
@@ -0,0 +1,15 @@
+﻿using DotNetNuke.Web.Api;
+using System.Net;
+
+namespace DotNetNuke.Security.Tls12
+{
+    public class ServiceRouteMapper : IServiceRouteMapper
+    {
+        public void RegisterRoutes(IMapRoute mapRouteManager)
+        {
+
+            // Enable TLS 1.2
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12; // .NET 4.5
+        }
+    }
+}
